452144ab64d59810deea3a79694ca6a5ec50a328
mingrammer <k239507@gmail.com>
2016-09-23 02:48:33 +0900

    Refactoring the functional tests by removing the duplicates code

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -33,9 +38,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # There is additional text box to add a item
         # Type the other task
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy a bike', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy a bike')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy a car')
@@ -43,10 +46,8 @@ class NewVisitorTest(unittest.TestCase):
 
         # The page will be reloaded again
         # There are two items in list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy a bike', [row.text for row in rows])
-        self.assertIn('2: Buy a car', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy a bike')
+        self.check_for_row_in_list_table('2: Buy a car')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
 

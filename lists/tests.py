8d03dc49f0666a3488f681c79e9e8eff583049c1
mingrammer <k239507@gmail.com>
2016-09-23 15:04:42 +0900

    Create 'Item' model and write test for that

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -53,3 +54,25 @@ class HomePageTest(TestCaseWithCSRF):
         )
 
         self.assertEqualExceptCSRF(response.content.decode(), expected_html)
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'First item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'First item')
+        self.assertEqual(second_saved_item.text, 'Second item')

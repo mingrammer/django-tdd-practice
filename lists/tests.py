070b8721306637f9bbdaaebf8f2914b7db9548bb
mingrammer <k239507@gmail.com>
2016-09-24 10:50:59 +0900

    Redirect after post request and show all added items on template

--- lists/tests.py
+++ lists/tests.py
@@ -43,17 +43,40 @@ class HomePageTest(TestCaseWithCSRF):
         request.method = 'POST'
         request.POST['item_text'] = 'New task item'
 
+        home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+
+        new_item = Item.objects.first()
+
+        self.assertEqual(new_item.text, 'New task item')
+
+    def test_home_page_saves_items_when_necessary(self):
+        request = HttpRequest()
+
+        home_page(request)
+
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_redirects_after_post(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'New task item'
+
         response = home_page(request)
 
-        self.assertIn('New task item', response.content.decode())
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
 
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'New task item'},
-            request=request
-        )
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='item1')
+        Item.objects.create(text='item2')
 
-        self.assertEqualExceptCSRF(response.content.decode(), expected_html)
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('item1', response.content.decode())
+        self.assertIn('item2', response.content.decode())
 
 
 class ItemModelTest(TestCase):

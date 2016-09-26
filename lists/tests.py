d0ec3665a53b750321d96f6283c2a6191685f968
mingrammer <k239507@gmail.com>
2016-09-26 15:31:42 +0900

    Create List model

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 
@@ -39,15 +39,20 @@ class HomePageTest(TestCaseWithCSRF):
         self.assertEqualExceptCSRF(response.content.decode(), expected_html)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'First item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Second item'
+        second_item.list = list_
         second_item.save()
 
         saved_items = Item.objects.all()
@@ -58,7 +63,9 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
 
         self.assertEqual(first_saved_item.text, 'First item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Second item')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -69,8 +76,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='item1')
-        Item.objects.create(text='item2')
+        list_ = List.objects.create()
+        Item.objects.create(text='item1', list=list_)
+        Item.objects.create(text='item2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 

c36162bc72c9e9cfbfdf3b82a33f31bd4c51b2b9
mingrammer <k239507@gmail.com>
2016-09-26 13:37:38 +0900

    Add new url, view, template for printing list

--- lists/tests.py
+++ lists/tests.py
@@ -66,17 +66,7 @@ class HomePageTest(TestCaseWithCSRF):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='item1')
-        Item.objects.create(text='item2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('item1', response.content.decode())
-        self.assertIn('item2', response.content.decode())
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
 
 
 class ItemModelTest(TestCase):
@@ -99,3 +89,20 @@ class ItemModelTest(TestCase):
 
         self.assertEqual(first_saved_item.text, 'First item')
         self.assertEqual(second_saved_item.text, 'Second item')
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='item1')
+        Item.objects.create(text='item2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'item1')
+        self.assertContains(response, 'item2')

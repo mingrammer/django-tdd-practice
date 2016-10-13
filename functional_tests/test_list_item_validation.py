f004230cec54dba5a0c9a36b46595a1f618c3afb
mingrammer <k239507@gmail.com>
2016-10-13 17:03:48 +0900

    Add item validation test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,13 +5,26 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith go main page and will add empty item by mistake
         # Press enter key when input box is empty
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # Page is reloaded and show error message that can not add empty item
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, 'Can not register empty item')
 
         # Typing other item is processed correctly
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.check_for_row_in_list_table('1: Buy milk')
 
         # She add empty item again intentionally
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # Error message is shown again in list page
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, 'Can not register empty item')
 
-        self.fail('Write me!')
+        # Register non-empty item is valid
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
